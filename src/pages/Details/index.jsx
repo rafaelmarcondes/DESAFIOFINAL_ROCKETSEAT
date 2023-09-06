32a83b5971329a4f4165cb36b3063489db6734e4
Rafael Marcondes <38266053+rafaelmarcondes@users.noreply.github.com>
2023-09-06 17:02:37 -0300

    Add files via upload

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,21 @@
+import { Container } from "./styles.js";
+
+import { Header } from "../../components/Header/index"
+import { Button } from "../../components/button/index";
+
+
+export function Details(){
+  return(
+
+    <Container>
+      <Header/>
+
+        <h1>Em construção</h1>
+
+  
+    <Button title="Voltar"/>
+
+    </Container>
+    
+  )
+}
